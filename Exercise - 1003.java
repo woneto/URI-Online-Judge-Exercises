3d26951afdcc160ebe3e0e997d35fe27bdd6590a
woneto <woliveiraneto1996@gmail.com>
2016-12-07 18:50:59 -0200

    Add files via upload

---
+++ Exercise - 1003.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+		Scanner entrada = new Scanner(System.in);
+		int a = entrada.nextInt();
+		int b = entrada.nextInt();
+		System.out.println("SOMA = " + (a+b));
+    }
+ 
+}
